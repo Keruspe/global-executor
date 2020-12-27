484bdf13a31a6332be51e2843457c33a44ca2c32
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-12-27 14:03:38 +0100

    fix tokio
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- tests/tokio.rs
+++ tests/tokio.rs
@@ -3,5 +3,5 @@ mod tester;
 #[test]
 fn test_tokio() {
     // FIXME: make tokio spawn_local work
-    tester::test_global_executor(tokio_executor_trait::Tokio, false);
+    tester::test_global_executor(tokio_executor_trait::Tokio::default(), false);
 }
